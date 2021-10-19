eee337db3c25f439bd422fb5ff807ee8d0e409df
SikieikiYamada <1852532209@qq.com>
2021-10-19 20:37:14 +0800

    first commit

---
+++ src/main/java/com/internetplus/bankpunishment/BankPunishmentApplication.java
@@ -0,0 +1,13 @@
+package com.internetplus.bankpunishment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankPunishmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankPunishmentApplication.class, args);
+	}
+
+}
